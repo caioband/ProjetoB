a32d5a5ba798952cc5209a87745d8ee22834346c
caioband <caioobsantos@gmail.com>
2024-09-26 22:08:14 -0300

    danilo gay

---
+++ src/main/java/com/barbearia/projeto/B/ProjetoBApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.projeto.B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbearia/projeto/B/ProjetoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.projeto.B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
